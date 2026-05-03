21974e377fa0e27a17f5e355e7b8539b59f75cb1
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2026-05-03 11:45:20 -0700

    Wire taskweft-nif as a git dep, remove extracted files

--- mix.exs
+++ mix.exs
@@ -6,7 +6,6 @@ defmodule Taskweft.MixProject do
       app: :taskweft,
       version: "0.1.0",
       elixir: "~> 1.17",
-      compilers: [:elixir_make] ++ Mix.compilers(),
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
       dialyzer: [plt_add_apps: [:mix], ignore_warnings: ".dialyzer_ignore.exs"]
@@ -26,8 +25,7 @@ defmodule Taskweft.MixProject do
 
   defp deps do
     [
-      {:elixir_make, "~> 0.9"},
-      {:jason, "~> 1.4"},
+      {:taskweft_nif, github: "V-Sekai-fire/taskweft-nif"},
       {:ex_mcp, "~> 0.9"},
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
       {:propcheck, "~> 1.4", only: [:test, :dev], runtime: false},
