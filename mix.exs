4917e3de7d7950306873b664fdd21425b37302f2
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2026-07-14 17:36:12 -0700

    Add ex_doc for Hex docs publishing (#14)

--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule Taskweft.MCP.MixProject do
       {:jason, "~> 1.4"},
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
       {:propcheck, "~> 1.4", only: [:test, :dev], runtime: false},
-      {:mox, "~> 1.2", only: :test}
+      {:mox, "~> 1.2", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
